846247049df291b4ea85e2d90f3cd809b487ebb4
cminnerath <cminnerath@gmail.com>
2015-09-16 17:10:33 -0600

    initializes with district (pairing)

--- lib/statewide_testing.rb
+++ lib/statewide_testing.rb
@@ -4,8 +4,13 @@ require_relative 'statewide_testing_loader'
 class StateWideTesting
   attr_accessor :state
 
-  def initialize
+  def initialize(name = nil)
     @state = StateWideTestingLoader.new
     @state.load_all
+    @state.third_grade_proficiency.select! { |hash| parse(hash, name)}
+  end
+
+  def parse(hash, name)
+     hash[:location] == name
   end
 end
--- test/statewide_testing_test.rb
+++ test/statewide_testing_test.rb
@@ -8,4 +8,15 @@ class StateWideTestingTest < Minitest::Test
     testload = StateWideTesting.new
     assert Hash, testload.state.third_grade_proficiency.first
   end
+
+  def test_it_can_initialize_with_a_district_name
+    testload = StateWideTesting.new('WOODLAND PARK RE-2')
+    testvar = testload.state.third_grade_proficiency
+    assert_equal 'WOODLAND PARK RE-2', testvar.first[:location]
+    assert_equal 'WOODLAND PARK RE-2', testvar.last[:location]
+    testload = StateWideTesting.new('AKRON R-1')
+    testvar = testload.state.third_grade_proficiency
+    assert_equal 'AKRON R-1', testvar.first[:location]
+    assert_equal 'AKRON R-1', testvar.last[:location]
+  end
 end
