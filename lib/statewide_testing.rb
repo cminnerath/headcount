3748dcc11dc6cac39b0b4cf2cc5715829fb3eb9a
cminnerath <cminnerath@gmail.com>
2015-09-17 13:32:55 -0600

    proficient by grade functional (pairing)

--- lib/statewide_testing.rb
+++ lib/statewide_testing.rb
@@ -1,16 +1,43 @@
 require 'csv'
 require_relative 'statewide_testing_loader'
 
+class Array
+
+end
+
 class StateWideTesting
-  attr_accessor :state
+  attr_accessor :state, :prof, :output
 
   def initialize(name = nil)
     @state = StateWideTestingLoader.new
     @state.load_all
-    @state.third_grade_proficiency.select! { |hash| parse(hash, name)}
+    @third_grade = @state.third_grade_proficiency.select! { |hash| parse(hash, name)}
+    @eighth_grade = @state.eighth_grade_proficiency.select! { |hash| parse(hash, name)}
   end
 
   def parse(hash, name)
      hash[:location] == name
   end
+
+  def proficient_by_grade(grade = nil)
+    if grade == 3
+       data_by_year = @third_grade.group_by { |data| data[:timeframe] }
+       @output = {}
+       data_by_year.each do |k, v|
+         scores = []
+         v.each { |hash| scores << {hash[:score].downcase.to_sym => hash[:data].to_f}}
+         @output[k.to_i] = scores[0].merge(scores[1]).merge(scores[2])
+       end
+     elsif grade == 8
+       data_by_year = @eighth_grade.group_by { |data| data[:timeframe] }
+       @output = {}
+       data_by_year.each do |k, v|
+         scores = []
+         v.each { |hash| scores << {hash[:score].downcase.to_sym => hash[:data].to_f}}
+         @output[k.to_i] = scores[0].merge(scores[1]).merge(scores[2])
+       end
+     else
+         @output =  "Error: Grade Not Found"
+     end
+   end
 end
--- test/statewide_testing_test.rb
+++ test/statewide_testing_test.rb
@@ -19,4 +19,36 @@ class StateWideTestingTest < Minitest::Test
     assert_equal 'AKRON R-1', testvar.first[:location]
     assert_equal 'AKRON R-1', testvar.last[:location]
   end
+  
+  def setup
+  @third_grade_results =  {2008=>{:math=>0.798, :reading=>0.838, :writing=>0.556},
+                           2009=>{:math=>0.769, :reading=>0.799, :writing=>0.616},
+                           2010=>{:math=>0.715, :reading=>0.76, :writing=>0.515},
+                           2011=>{:math=>0.708, :reading=>0.739, :writing=>0.503},
+                           2012=>{:reading=>0.824, :math=>0.818, :writing=>0.59893},
+                           2013=>{:math=>0.72, :reading=>0.78035, :writing=>0.50575},
+                           2014=>{:math=>0.7006, :reading=>0.76364, :writing=>0.51497}}
+
+  @eighth_grade_results = {2008=>{:math=>0.523, :reading=>0.795, :writing=>0.636},
+                           2009=>{:math=>0.59, :reading=>0.748, :writing=>0.573},
+                           2010=>{:math=>0.639, :reading=>0.813, :writing=>0.0},
+                           2011=>{:reading=>0.79412, :math=>0.59244, :writing=>0.67647},
+                           2012=>{:math=>0.52239, :writing=>0.56219, :reading=>0.79104},
+                           2013=>{:math=>0.53589, :reading=>0.76555, :writing=>0.66029},
+                           2014=>{:math=>0.54976, :reading=>0.70283, :writing=>0.58962}}
+  end
+
+  def test_it_can_find_proficiency_in_a_district_by_grade
+    testload = StateWideTesting.new('WOODLAND PARK RE-2')
+    testvar = testload.proficient_by_grade(3)
+    assert_equal @third_grade_results, testload.output
+    testload.proficient_by_grade(8)
+    assert_equal @eighth_grade_results, testload.output
+    testload.proficient_by_grade(9)
+    assert_equal "Error: Grade Not Found", testload.output
+    testload.proficient_by_grade("a")
+    assert_equal "Error: Grade Not Found", testload.output
+    testload.proficient_by_grade
+    assert_equal "Error: Grade Not Found", testload.output
+  end
 end
